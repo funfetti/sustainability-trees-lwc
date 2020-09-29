37c27176490b8a4672648497061410633b88be69
Stephanie Ho <stephanie.e.ho@gmail.com>
2020-09-29 11:52:47 -0500

    initial upload

---
+++ force-app/main/default/lwc/trees/trees.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+
+export default class Trees extends LightningElement {
+    openTrillion(){
+        window.open("https://trees.salesforce.com/", "_blank")
+    }
+}
